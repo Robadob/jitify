b7478588ca58b9694171f17337dd261d6a06a363
Ben Barsdell <bbarsdell@nvidia.com>
2023-11-21 21:13:52 +1100

    Fix StringRef issue in path_is_absolute for C++17

--- jitify2.hpp
+++ jitify2.hpp
@@ -2297,7 +2297,7 @@ inline std::string path_base(const std::string& p) {
   }
 }
 
-inline bool path_is_absolute(const std::string& p) {
+inline bool path_is_absolute(StringRef p) {
 #if defined _WIN32 || defined _WIN64
   return (p.size() >= 1 && (p[0] == '\\' || p[0] == '/')) ||
          (p.size() >= 3 && p[1] == ':' && (p[2] == '\\' || p[2] == '/'));
@@ -3735,8 +3735,9 @@ inline void copy_compiler_flag_for_linker_ptxas(
   for (int idx : idxs) {
     const Option& compiler_option = compiler_options[idx];
     const Option linker_option =
-        output_key.empty() ? compiler_option
-                           : Option(output_key, compiler_option.value());
+        output_key.empty()
+            ? compiler_option
+            : Option(std::string(output_key), compiler_option.value());
     linker_options->push_back(linker_option);
   }
 }
